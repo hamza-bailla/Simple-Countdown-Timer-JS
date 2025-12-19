af599107cc577c9f2e39902dd3698cec88393c35
Hamza BAILLA <133886011+hamza-bailla@users.noreply.github.com>
2025-12-19 10:14:11 +0100

    Update set.js

--- set.js
+++ set.js
@@ -1,91 +1,3 @@
-// const start = document.querySelector('#ajouBtn');
-// const inputNumber = document.querySelector('#timer');
-// const rest = document.querySelector('#resett');
-// const pause = document.querySelector('#pause');
-// var valueNum = 0;
-
-// start.addEventListener('click',clickhandle);
-// var resiit=0;//variable pour prendre la valeur de Horloge declarer dans clickhandle() et jel'utilise pour clearhandle(horloge);
-// var pausee=0;
-
-// // 1. Move 'compter' to the global scope so it remembers its value between clicks
-// let compter = 0; 
-// let horloge = null; // Store the interval here
-
-// function clickhandle() {
-//     // 2. Only pull from input if the counter is currently at 0 (a fresh start)
-//     if (compter <= 0) {
-//         if (inputNumber.value === "") {
-//             alert("Please enter a number");
-//             return;
-//         }
-//         compter = parseInt(inputNumber.value);
-//     }
-
-//     // 3. Prevent multiple timers from running at once
-//     if (horloge !== null) return; 
-
-//     horloge = setInterval(() => {
-//         compter--;
-//         console.log("the counter is: " + compter);
-        
-//         // Update display
-//         totalDisplay.innerText = ` ${compter}`;
-
-//         if (compter <= 0) {
-//             clearInterval(horloge);
-//             horloge = null;
-//         }
-//     }, 1000);
-    
-//     // Save the interval ID to resiit so reset/pause work
-//     resiit = horloge;
-//     function updateTotalDisplay() {
-//         totalDisplay.innerText = ` ${compter}`;
-//         //pausee = compter;
-//     }
-// }
-
-// function clickpause() {
-//     clearInterval(resiit);
-//     horloge = null; // Reset this so the 'Start' button knows it can restart
-// } 
-    
-//     // --- Fonction pour mettre à jour l'affichage (Définissez-la en dehors de clickhandle pour la réutiliser !)
-    
-
-
-// rest.addEventListener('click',clickreset);
-// const totalDisplay = document.getElementById('total-display');
-// function clickreset(){
-//         clearInterval(resiit);
-//        totalDisplay.innerText ="Ooops rest clicked";
-// }
-// //----------------------------------Pause------------------------
-
-// pause.addEventListener('click',clickpause);
-// const pauseTostart = resiit;
-
-// function clickpause(){
-    
-//     clearInterval(resiit);
-// };
-
-// if(pause.addEventListener('click',clickpause)){
-//     const pauseTostart=setInterval(()=>{
-//         newCompter--;
-//         //pausee = compter;
-//         if(compter === 0 ){
-//             clearInterval(pauseTostart);
-//         }
-//         console.log("the copter is: "+newCompter);
-//         updateTotalDisplay();
-        
-//     },1000);
-// }
-
-
-// 1. Select DOM elements
 const startBtn = document.querySelector('#ajouBtn');
 const inputNumber = document.querySelector('#timer');
 const resetBtn = document.querySelector('#resett');
@@ -146,6 +58,7 @@ function clickreset() {
     inputNumber.value = ""; 
     totalDisplay.innerText = "RESET";
 }
+
 
 
 
